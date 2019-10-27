5bfc41ed61dda27c7a953d2472419e73c0d8be73
NickM <nmikstas@alumni.scu.edu>
2019-10-27 16:44:04 -0600

    Separated the game code out from the inquirer code

---
+++ gameEngine.js
@@ -0,0 +1,107 @@
+let WordGen    = require("./wordGen.js");
+let Word       = require("./word.js");
+let colors     = require("colors");
+
+//This is where the guts of the game are stored.
+let GameEngine = function(fileName, inputCallback)
+{
+    this.NUM_GUESSES   = 9; 
+    this.inputCallback = inputCallback;
+    this.fileName      = fileName;
+    this.guessesLeft   = this.NUM_GUESSES;
+    this.badGuesses    = [];
+    this.goodGuesses   = [];
+    this.wins          =  0;
+    this.losses        =  0;
+    this.wordGen;
+    this.thisWord;
+
+    let self = this;
+    this.runGame = function()
+    {
+        //Pick a new random word.
+        self.thisWord = new Word(self.wordGen.getWord());
+        self.guessesLeft = self.NUM_GUESSES;
+        self.badGuesses  = [];
+        self.goodGuesses = [];
+        
+        console.log("\n************** New Word **************".yellow);
+        console.log(self.thisWord.getWord() + "\n");
+
+        //Get the user's letter guess.
+        setTimeout(self.inputCallback, 0); //Do this to prevent filling up the stack.    
+    }
+
+    this.processInput = function(guessedLetter)
+    {
+        //Record how many underscores are left before the guess.
+        //Then make a guess and check to see if the number of 
+        //underscores changed.
+        let lastUnderscores = this.thisWord.countUnderscores();
+        this.thisWord.guessLetter(guessedLetter.guess);
+        let thisUnderscores = this.thisWord.countUnderscores();
+
+        if(!thisUnderscores) //Check for a win.
+        {
+            this.wins++;
+            console.log("\n************** You Win! **************".green);
+            console.log(this.thisWord.getWord());
+            console.log("Wins:   " + this.wins.toString().magenta);
+            console.log("Losses: " + this.losses.toString().magenta);
+            setTimeout(this.runGame, 0); //Do this to prevent filling up the stack.
+        }
+        else //Check the other conditions.
+        {
+            //Check if the player correctly guessed a letter.
+            if(lastUnderscores !== thisUnderscores)
+            {
+                console.log("\n**************** Hit! ****************".yellow);
+                console.log(this.thisWord.getWord());
+                console.log("Bad guesses:  " + this.badGuesses.toString().cyan);
+                console.log("guesses left: " + this.guessesLeft.toString().cyan + "\n");
+                this.goodGuesses.push(guessedLetter.guess)
+                setTimeout(this.inputCallback, 0); //Do this to prevent filling up the stack.
+            }
+            //Check if the letter has already been guessed.
+            else if(this.badGuesses.includes(guessedLetter.guess) || 
+                    this.goodGuesses.includes(guessedLetter.guess))
+            {
+                console.log("\n********** Already Guessed! **********".yellow);
+                console.log(this.thisWord.getWord());
+                console.log("Bad guesses:  " + this.badGuesses.toString().cyan);
+                console.log("guesses left: " + this.guessesLeft.toString().cyan + "\n");
+                setTimeout(this.inputCallback, 0); //Do this to prevent filling up the stack.
+            }
+            else
+            {
+                //Wrong guess and letter not already guessed.
+                this.guessesLeft--;
+                this.badGuesses.push(guessedLetter.guess)
+
+                if(!this.guessesLeft) //Check for a loss.
+                {
+                    this.losses++;
+                    console.log("\n************* You Lose! **************".red);
+                    console.log(this.thisWord.revealWord());
+                    console.log("Wins:   " + this.wins.toString().magenta);
+                    console.log("Losses: " + this.losses.toString().magenta);
+                    setTimeout(this.runGame, 0); //Do this to prevent filling up the stack.
+                }
+                else //Keep playing.
+                {
+                    console.log("\n*************** Miss! ****************".yellow);
+                    console.log(this.thisWord.getWord());
+                    console.log("Bad guesses:  " + this.badGuesses.toString().cyan);
+                    console.log("guesses left: " + this.guessesLeft.toString().cyan + "\n");
+                    setTimeout(this.inputCallback, 0); //Do this to prevent filling up the stack.
+                }
+            }
+        }
+    }
+
+    console.log("\n\n\n-----Node.js Word Guess Game-------");
+    console.log("type " + "quit".red + " or " + "exit".red + " to leave the game");
+    this.wordGen = new WordGen(this.fileName, this.runGame);
+}
+
+module.exports = GameEngine;
--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-let WordGen  = require("./wordGen.js");
-let Word     = require("./word.js");
-let inquirer = require("inquirer");
-let colors   = require("colors");
-
-const numGuesses = 9;
-
-let wordGen;
-let thisWord;
-let badGuesses  = [];
-let goodGuesses = [];
-let wins        =  0;
-let losses      =  0;
-let guessesLeft = numGuesses;
+let GameEngine = require("./gameEngine.js");
+let inquirer   = require("inquirer");
 
 let getInput = function()
 {
@@ -40,93 +28,15 @@ let getInput = function()
         }
     ]).then(function(guessedLetter)
     {
-        //Record how many underscores are left before the guess.
-        //Then make a guess and check to see if the number of 
-        //underscores changed.
-        let lastUnderscores = thisWord.countUnderscores();
-        thisWord.guessLetter(guessedLetter.guess);
-        let thisUnderscores = thisWord.countUnderscores();
-
-        if(!thisUnderscores) //Check for a win.
-        {
-            wins++;
-            console.log("\n************** You Win! **************".green);
-            console.log(thisWord.getWord());
-            console.log("Wins:   " + wins.toString().magenta);
-            console.log("Losses: " + losses.toString().magenta);
-            setTimeout(runGame, 0); //Do this to prevent filling up the stack.
-        }
-        else //Check the other conditions.
-        {
-            //Check if the player correctly guessed a letter.
-            if(lastUnderscores !== thisUnderscores)
-            {
-                console.log("\n**************** Hit! ****************".yellow);
-                console.log(thisWord.getWord());
-                console.log("Bad guesses:  " + badGuesses.toString().cyan);
-                console.log("guesses left: " + guessesLeft.toString().cyan + "\n");
-                goodGuesses.push(guessedLetter.guess)
-                setTimeout(getInput, 0); //Do this to prevent filling up the stack.
-            }
-            //Check if the letter has already been guessed.
-            else if(badGuesses.includes(guessedLetter.guess) || goodGuesses.includes(guessedLetter.guess))
-            {
-                console.log("\n********** Already Guessed! **********".yellow);
-                console.log(thisWord.getWord());
-                console.log("Bad guesses:  " + badGuesses.toString().cyan);
-                console.log("guesses left: " + guessesLeft.toString().cyan + "\n");
-                setTimeout(getInput, 0); //Do this to prevent filling up the stack.
-            }
-            else
-            {
-                //Wrong guess and letter not already guessed.
-                guessesLeft--;
-                badGuesses.push(guessedLetter.guess)
-
-                if(!guessesLeft) //Check for a loss.
-                {
-                    losses++;
-                    console.log("\n************* You Lose! **************".red);
-                    console.log(thisWord.revealWord());
-                    console.log("Wins:   " + wins.toString().magenta);
-                    console.log("Losses: " + losses.toString().magenta);
-                    setTimeout(runGame, 0); //Do this to prevent filling up the stack.
-                }
-                else //Keep playing.
-                {
-                    console.log("\n*************** Miss! ****************".yellow);
-                    console.log(thisWord.getWord());
-                    console.log("Bad guesses:  " + badGuesses.toString().cyan);
-                    console.log("guesses left: " + guessesLeft.toString().cyan + "\n");
-                    setTimeout(getInput, 0); //Do this to prevent filling up the stack.
-                }
-            }
-        }
+        gameEngine.processInput(guessedLetter);
     });
 }
 
-let runGame = function()
-{
-    //Pick a new random word.
-    thisWord = new Word(wordGen.getWord());
-    badGuesses  = [];
-    goodGuesses = [];
-    guessesLeft = numGuesses;
-
-    console.log("\n************** New Word **************".yellow);
-    console.log(thisWord.getWord() + "\n");
-
-    //Get the user's letter guess.
-    setTimeout(getInput, 0); //Do this to prevent filling up the stack.    
-}
-
 //The game execution starts here.
-console.log("\n\n\n-----Node.js Word Guess Game-------");
-console.log("type " + "quit".red + " or " + "exit".red + " to leave the game");
 try
 {
-    //Try to instatiate the word generator object.
-    wordGen = new WordGen("./phrases.txt", runGame);
+     //Try to instatiate the game engine object.
+    gameEngine = new GameEngine("./phrases.txt", getInput);
 }
 catch(error)
 {
